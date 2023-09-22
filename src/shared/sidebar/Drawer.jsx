21d12e532209b36cdee8612f72fd17c5a90228d7
Franco Fresno <franco.fresno@gmail.com>
2023-09-22 16:48:19 -0300

    Agrego sidebar button

---
+++ src/shared/sidebar/Drawer.jsx
@@ -0,0 +1,40 @@
+import { styled } from "@mui/material/styles";
+import MuiDrawer from "@mui/material/Drawer";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+export const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-start",
+  padding: theme.spacing(0, 1),
+  ...theme.mixins.toolbar,
+}));
+
+export const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== "open" })(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
--- src/shared/sidebar/SidebarButton.jsx
+++ src/shared/sidebar/SidebarButton.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Button } from "@mui/material";
 
-export const SidebarButton = ({ icon, label, isFocused, handleClick }) => {
-  return <div>SidebarButton</div>;
+const buttonStyles = {
+  textTransform: "none",
+  opacity: 0.65,
+  color: "#FFF",
+  fontSize: "16px",
+  alignItems: "flex-start",
+  "&:hover": { backgroundColor: "transparent", opacity: 1 },
+};
+
+export const SidebarButton = ({ label, isActive, isExpanded, handleClick, children }) => {
+  return (
+    <Button startIcon={children} onClick={handleClick} sx={buttonStyles} disableElevation>
+      {isExpanded && label}
+    </Button>
+  );
 };
